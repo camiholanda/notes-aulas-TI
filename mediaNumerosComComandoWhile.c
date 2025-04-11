ccb9957d7007f4a0cd2fb9fe67e131ce7b0ea045
Camila Holanda <cmdhn.holanda@gmail.com>
2025-04-11 09:30:38 -0300

    Add files via upload

---
+++ mediaNumerosComComandoWhile.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+        int n, i = 1;
+        float num, soma, media;
+
+        printf("Informe a quantidade de numeros que ira digitar: \n");
+        scanf("%d", &n);
+
+		while(i <= n)
+		{
+             printf("Digite um número: ");
+             scanf("%f", &num);
+             soma = soma + num;
+             i++;
+        }
+            media = soma / n;
+            printf("A media dos numeros eh: %f ", media);
+
+            return 0;
+
+}
