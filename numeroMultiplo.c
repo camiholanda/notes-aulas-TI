556afa54266fc5b518c97b63ff735b5c86a5065b
Camila Holanda <cmdhn.holanda@gmail.com>
2025-04-17 09:02:57 -0300

    Create numeroMultiplo.c

---
+++ numeroMultiplo.c
@@ -0,0 +1,30 @@
+programa
+{
+	
+	funcao inicio()
+	{
+		inteiro num, i = 0, result
+		
+		escreva("Digite um numero: ")
+		leia(num)
+
+		enquanto(num != 0)
+		{
+			enquanto(num < 0)
+			{
+				escreva("Numero invalido. Digite outro numero: ")
+				leia(num)
+			}
+		se(num % i == 0)
+		{
+			result = num * 1
+			result++
+			escreva(result)
+		
+		}
+
+
+			
+			}
+		}
+}
