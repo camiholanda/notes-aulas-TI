72dec6853059f0b1507ae3c40d6639571162512e
Camila Holanda <cmdhn.holanda@gmail.com>
2025-06-12 12:06:59 -0300

    Add files via upload

---
+++ exerciciosLinguagemC/exerc1-StructLingC-ProntuarioNotasAluno.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct registroAluno{
+        char registro[50];
+        float nota1, nota2, nota3;
+};
+
+int main()
+{
+        struct registroAluno a1;
+
+        printf("Informe seu registro:");
+        setbuf(stdin, NULL);
+		gets(a1.registro);
+
+      	printf("Informe sua nota1:");
+		scanf("%f", &a1.nota1);
+
+		printf("Informe sua nota2:");
+		scanf("%f", &a1.nota2);
+
+		printf("Informe sua nota3:");
+		scanf("%f", &a1.nota3);
+
+}
+
