85bd98826491acc1ca54fc84ce3dfcb320aef428
Camila Holanda <cmdhn.holanda@gmail.com>
2026-03-25 09:35:44 -0300

    Update notes-250326-node.js

--- notes-250326-node.js
+++ notes-250326-node.js
@@ -1,4 +1,25 @@
+SOBRE JSON
 
 dependências são pacotes que o sistema usa de outros sistemas, como import bibliotecas ??
 
         npm install 
+
+
+SINTAXE de lista com colchetes []
+exemplo:
+	{
+	    "turma": "3o INFO 2026.1",
+	    "numero-alunos": 12,
+	    "alunos": [
+	        {
+	           "nome": "camila",
+	           "idade": 25
+	        },
+	        {
+	           "nome": "camila",
+	           "idade": 20
+	        }
+	    ]
+	}
+
+HTML >>>> comando div#painel 
