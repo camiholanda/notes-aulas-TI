a00114cea375763a49a33181fbb78a61aea3e9c6
Camila Holanda <cmdhn.holanda@gmail.com>
2026-04-01 09:22:07 -0300

    Document Node.js project setup and dependency installation
    Added instructions for initializing a Node.js project and installing dependencies.

--- notes-250326-node.js
+++ notes-250326-node.js
@@ -22,4 +22,25 @@ exemplo:
 	    ]
 	}
 
-HTML >>>> comando div#painel 
+HTML >>>> comando div#painel
+
+
+
+abrir terminal vscode
+command prompt >> abrir terminal dentro do vs
+	verificar node instalado
+digitar comando npm init -y
+e assim cria-se o primeiro pacote "package.json"
+
+npm >> para inicializar o projeto e pode ser usado para instalar depedências
+
+npm se conecta numa base de pacotes e procura os pacotes indicados, encontrado os pacotes ele instala
+
+comando para criar package-lock.json e node_modules:
+	>>   npm install express ejs
+
+
+
+
+
+
